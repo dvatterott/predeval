8b81abc6eab2b837d2fcdcf65c0551d167dd2e81
Dan Vatterott <dvatterott@gmail.com>
2019-01-03 15:13:49 -0600

    documentation help

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ import predeval
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -44,7 +44,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon'
 source_suffix = '.rst'
 
 # The master toctree document.
-# master_doc = 'index'
+master_doc = 'index'
 
 # General information about the project.
 project = u'predeval'
@@ -95,7 +95,7 @@ todo_include_todos = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
